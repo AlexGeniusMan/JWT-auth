bf681f3b65790971bbb72158f4e61cebc32c7326
AlexGeniusMan <alexgeniusman@gmail.com>
2021-01-12 11:02:37 +0300

    user registration added

--- project/urls.py
+++ project/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from django.contrib import admin
+from users.urls import urlpatterns as users_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'^admin/', admin.site.urls),
+    # url(r'^user/', include('users.urls', namespace='users')),
+
+] + users_urlpatterns
